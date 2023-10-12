4d28805d75d19639378040106716edfe5008f064
Alexey Silaenkov <silaenckov2014@yandex.ru>
2023-10-12 20:36:15 +0300

    edit DZ4

--- DZ4/src/test/java/BookRepositoryMockTest.java
+++ DZ4/src/test/java/BookRepositoryMockTest.java
@@ -1,7 +1,12 @@
+import org.example.Book;
 import org.example.BookRepository;
 import org.example.BookService;
 import org.example.InMemoryBookRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -11,14 +16,24 @@ public class BookRepositoryMockTest {
     @Test
     void  test(){
 
-        InMemoryBookRepository dummyInMemoryBookRepository = mock(InMemoryBookRepository.class);
+        BookRepository dummyBookRepository = mock(BookRepository.class);
+        Book book1 = new Book("1", "Book1", "Author1");
+        Book book2 = new Book("1", "Book1", "Author1");
+        List<Book> books = new ArrayList<>();
+        books.add(book1);
+        books.add(book2);
 
-//        InMemoryBookRepository IMBR = new InMemoryBookRepository();
-        BookService bookService = new BookService(dummyInMemoryBookRepository);
-        when();
+        when(dummyBookRepository.findById("1")).thenReturn(book1);
+        when(dummyBookRepository.findAll()).thenReturn(books);
+
+        BookService bookService = new BookService(dummyBookRepository);
+
+        Assertions.assertEquals(bookService.findBookById("1"), book1);
+        Assertions.assertEquals(bookService.findAllBooks(), books);
 
 
-        System.out.println(bookService.findBookById("1").getAuthor());
+
+
 
     }
 }
